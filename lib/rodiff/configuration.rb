f147ee4edaeb1a0266c2624eb7469f2538cc6696
Ryan Chang <ryancyq@gmail.com>
2026-02-08 23:36:47 +0800

    feat: rodiff config file (#15)

--- lib/rodiff/configuration.rb
+++ lib/rodiff/configuration.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require "yaml"
 require "rodiff/error"
 require "rodiff/executable"
+require "rodiff/helpers/file_finder"
 
 module Rodiff
   def self.configuration
@@ -14,6 +16,15 @@ module Rodiff
 
   class Configuration
     class UnknownConfiguration < Rodiff::Error; end
+
+    DOTFILE = ".rodiff.yml"
+    GEM_ROOT = if RUBY_VERSION >= "3.1"
+                 File.dirname(__FILE__, 3).freeze
+               else
+                 File.expand_path(File.join(File.dirname(__FILE__), "..", "..")).freeze
+               end
+    DEFAULT_CONFIG = File.join(GEM_ROOT, "config", "default.yml").freeze
+    SEARCH_ROOT = Dir.home.freeze
 
     ATTRS = Module.new.tap { |mod| include mod }
     READER_ATTRS = {
@@ -67,6 +78,9 @@ module Rodiff
 
       READER_ATTRS.each { |key, value| instance_variable_set("@#{key}", value) }
       ACCESSOR_ATTRS.each { |key, value| instance_variable_set("@#{key}", value) }
+
+      load_from_file(DEFAULT_CONFIG)
+      load_from_file(config_file_override) if config_file_override
     end
 
     def odiff_exe_path
@@ -84,6 +98,38 @@ module Rodiff
 
     private
 
+    def config_file_override(start_dir: Dir.pwd)
+      @config_file_override ||= begin
+        finder = Helpers::FileFinder.new("/")
+        finder.find_upwards(DOTFILE, start_dir, SEARCH_ROOT)
+      end
+    end
+
+    def load_from_file(path)
+      yml_config = YAML.load_file(path) if File.exist?(path)
+
+      if yml_config.is_a?(Hash)
+        yml_config.each do |key, value|
+          validate_config_key!(key.to_sym)
+          public_send("#{key}=", value) if respond_to?("#{key}=")
+        end
+      elsif empty_yml?(yml_config)
+        warn "Configuration file #{path} is empty"
+      else
+        warn "Configuration file #{path} must contain a Hash, got #{yml_config.class}"
+      end
+    rescue Psych::SyntaxError => e
+      raise Rodiff::Error, "Invalid YAML in #{path}: #{e.message}"
+    end
+
+    def empty_yml?(data)
+      # Ruby 2.7-3.0 with Psych 3.x
+      return data == false if RUBY_VERSION < "3.1"
+
+      # Ruby 3.1+ with Psych 4.x
+      data.nil?
+    end
+
     def validate_config_key!(key)
       return if READER_ATTRS.key?(key) || ACCESSOR_ATTRS.key?(key)
 
@@ -93,6 +139,41 @@ module Rodiff
     def value_for(key, &block)
       validate_config_key!(key)
       @config_overrides.fetch(key, &block)
+    end
+
+    def files_from_dir(dir)
+      included_files = files_from_glob(file_glob_pattern(dir, include_pattern))
+      excluded_files = files_from_glob(file_glob_pattern(dir, exclude_pattern))
+      (included_files - excluded_files).uniq
+    end
+
+    def files_from_glob(file_glob)
+      files = Dir.glob(file_glob)
+      files.map { |file| File.expand_path(file) }.sort
+    end
+
+    def file_glob_pattern(path, pattern)
+      trimmed = "{#{pattern.gsub(%r{\s*,\s*}, ",")}}"
+      return trimmed if pattern =~ %r{^(\./)?#{Regexp.escape(path)}} || absolute_pattern?(pattern)
+
+      File.join(path, trimmed)
+    end
+
+    def absolute_pattern?(pattern)
+      return pattern.start_with?(File::Separator) unless windows?
+      return false unless File::ALT_SEPARATOR
+
+      win_absolute_pattern = %r{\A\w+:#{Regexp.escape(File::ALT_SEPARATOR)}}.match?(pattern)
+      win_network_pattern = pattern.start_with?(File::ALT_SEPARATOR * 2)
+      win_absolute_pattern || win_network_pattern
+    end
+
+    def windows?
+      @windows ||= begin
+        require "rbconfig"
+        os = RbConfig::CONFIG["host_os"]
+        os.match?(%r{mingw})
+      end
     end
   end
 end
--- spec/rodiff/configuration_spec.rb
+++ spec/rodiff/configuration_spec.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "tmpdir"
+require "fileutils"
 
 require "rodiff/configuration"
 
 RSpec.describe Rodiff::Configuration do
-  let(:config) { described_class.new }
+  let(:config) do
+    # Create config without auto-discovery for isolated testing
+    instance = described_class.allocate
+    instance.instance_variable_set(:@config_overrides, {})
+    described_class::READER_ATTRS.each { |key, value| instance.instance_variable_set("@#{key}", value) }
+    described_class::ACCESSOR_ATTRS.each { |key, value| instance.instance_variable_set("@#{key}", value) }
+    instance
+  end
 
   describe "#odiff_exe_path" do
     it "resolves to default path" do
@@ -21,43 +30,249 @@ RSpec.describe Rodiff::Configuration do
     end
   end
 
-  describe "READER_ATTRS" do
+  describe "DOTFILE" do
+    subject { described_class::DOTFILE }
+
+    it { is_expected.to eq ".rodiff.yml" }
+  end
+
+  describe "GEM_ROOT" do
+    subject { described_class::GEM_ROOT }
+
+    it { is_expected.to eq Pathname.new(__dir__).dirname.parent.to_s }
+  end
+
+  describe "DEFAULT_CONFIG" do
+    subject { described_class::DEFAULT_CONFIG }
+
+    it { is_expected.to end_with("/config/default.yml") }
+  end
+
+  describe "SEARCH_ROOT" do
+    subject { described_class::SEARCH_ROOT }
+
+    it { is_expected.to eq Dir.home }
+  end
+
+  describe "read-only attributes" do
     described_class::READER_ATTRS.each do |key, value|
-      context "with ##{key}" do
-        it "reads" do
+      describe "##{key}" do
+        it "has default value" do
           expect(config.public_send(key)).to eq value
         end
 
-        context "when overrides" do
-          it "reads overrided value" do
-            expect(config.public_send(key)).to eq value
-            expect { config.overrides [[key, "overrided-value"]].to_h }.not_to raise_error
-            expect(config.public_send(key)).to eq "overrided-value"
-          end
+        it "can be overridden" do
+          expect(config.public_send(key)).to eq value
+          expect { config.overrides [[key, "overridden-value"]].to_h }.not_to raise_error
+          expect(config.public_send(key)).to eq "overridden-value"
         end
       end
     end
   end
 
-  describe "ACCESSOR_ATTRS" do
+  describe "configurable attributes" do
     described_class::ACCESSOR_ATTRS.each do |key, value|
-      context "with ##{key}" do
-        it "reads" do
+      describe "##{key}" do
+        it "has default value" do
           expect(config.public_send(key)).to eq value
         end
 
-        it "writes" do
+        it "can be set directly" do
           expect(config.public_send(key)).to eq value
-          expect { config.public_send("#{key}=", "random-value") }.not_to raise_error
-          expect(config.public_send(key)).to eq "random-value"
+          expect { config.public_send("#{key}=", "new-value") }.not_to raise_error
+          expect(config.public_send(key)).to eq "new-value"
         end
 
-        context "when overrides" do
-          it "reads overrided value" do
-            expect(config.public_send(key)).to eq value
-            expect { config.overrides [[key, "overrided-value"]].to_h }.not_to raise_error
-            expect(config.public_send(key)).to eq "overrided-value"
-          end
+        it "can be overridden" do
+          expect(config.public_send(key)).to eq value
+          expect { config.overrides [[key, "overridden-value"]].to_h }.not_to raise_error
+          expect(config.public_send(key)).to eq "overridden-value"
+        end
+      end
+    end
+  end
+
+  describe "#files_from_glob" do
+    it "returns files matching glob pattern" do
+      Dir.mktmpdir do |dir|
+        FileUtils.touch(File.join(dir, "test1.jpg"))
+        FileUtils.touch(File.join(dir, "test2.jpg"))
+        FileUtils.touch(File.join(dir, "test3.png"))
+
+        pattern = File.join(dir, "*.jpg")
+        files = config.send(:files_from_glob, pattern)
+
+        expect(files.length).to eq(2)
+        expect(files.all? { |f| f.end_with?(".jpg") }).to be true
+      end
+    end
+  end
+
+  describe "#files_from_dir" do
+    it "returns files matching include pattern" do
+      Dir.mktmpdir do |dir|
+        FileUtils.touch(File.join(dir, "test1.jpg"))
+        FileUtils.touch(File.join(dir, "test2.jpg"))
+        FileUtils.touch(File.join(dir, "test3.png"))
+
+        config.include_pattern = "*.jpg"
+        files = config.send(:files_from_dir, dir)
+
+        expect(files.length).to eq(2)
+        expect(files.all? { |f| f.end_with?(".jpg") }).to be true
+      end
+    end
+
+    it "excludes files matching exclude pattern" do
+      Dir.mktmpdir do |dir|
+        FileUtils.touch(File.join(dir, "keep1.jpg"))
+        FileUtils.touch(File.join(dir, "keep2.jpg"))
+        FileUtils.touch(File.join(dir, "ignore.jpg"))
+
+        config.include_pattern = "*.jpg"
+        config.exclude_pattern = "ignore.jpg"
+        files = config.send(:files_from_dir, dir)
+
+        expect(files.length).to eq(2)
+        expect(files.none? { |f| f.end_with?("ignore.jpg") }).to be true
+      end
+    end
+  end
+
+  describe "#file_glob_pattern" do
+    it "builds correct glob pattern" do
+      pattern = config.send(:file_glob_pattern, "/test/dir", "*.jpg")
+      expect(pattern).to eq("/test/dir/{*.jpg}")
+    end
+
+    it "handles multiple patterns" do
+      pattern = config.send(:file_glob_pattern, "/test/dir", "*.jpg, *.png")
+      expect(pattern).to eq("/test/dir/{*.jpg,*.png}")
+    end
+  end
+
+  describe "#absolute_pattern?" do
+    context "when on Unix-like systems" do
+      before { allow(config).to receive(:windows?).and_return(false) }
+
+      it "detects absolute paths" do
+        expect(config.send(:absolute_pattern?, "/absolute/path")).to be(true)
+        expect(config.send(:absolute_pattern?, "relative/path")).to be(false)
+      end
+    end
+
+    context "when on Windows systems" do
+      before do
+        allow(config).to receive(:windows?).and_return(true)
+        stub_const("File::ALT_SEPARATOR", "\\")
+      end
+
+      it "detects Windows absolute paths" do
+        expect(config.send(:absolute_pattern?, "C:\\path")).to be(true)
+      end
+
+      it "detects Windows network paths" do
+        expect(config.send(:absolute_pattern?, "\\\\server\\share")).to be(true)
+      end
+
+      it "detects relative paths" do
+        expect(config.send(:absolute_pattern?, "relative\\path")).to be(false)
+      end
+
+      it "handles missing File::ALT_SEPARATOR" do
+        stub_const("File::ALT_SEPARATOR", nil)
+        expect(config.send(:absolute_pattern?, "C:\\path")).to be(false)
+      end
+    end
+  end
+
+  describe "configuration file discovery" do
+    it "loads .rodiff.yml from current directory" do
+      Dir.mktmpdir do |dir|
+        config_content = <<~YAML
+          include_pattern: "auto/**/*.png"
+          color_threshold: 0.9
+        YAML
+
+        File.write(File.join(dir, ".rodiff.yml"), config_content)
+
+        Dir.chdir(dir) do
+          new_config = described_class.new
+          expect(new_config.include_pattern).to eq("auto/**/*.png")
+          expect(new_config.color_threshold).to eq(0.9)
+        end
+      end
+    end
+
+    it "loads .rodiff.yml from parent directory" do
+      Dir.mktmpdir do |dir|
+        config_content = <<~YAML
+          include_pattern: "parent/**/*.jpg"
+          ignore_antialiasing: true
+        YAML
+
+        File.write(File.join(dir, ".rodiff.yml"), config_content)
+
+        subdir = File.join(dir, "subdir")
+        FileUtils.mkdir_p(subdir)
+
+        Dir.chdir(subdir) do
+          new_config = described_class.new
+          expect(new_config.include_pattern).to eq("parent/**/*.jpg")
+          expect(new_config.ignore_antialiasing).to be(true)
+        end
+      end
+    end
+
+    it "uses default values when no config file found" do
+      Dir.mktmpdir do |dir|
+        Dir.chdir(dir) do
+          new_config = described_class.new
+          expect(new_config.include_pattern).to eq("{*,**/*}.jpg")
+          expect(new_config.color_threshold).to eq(0.1)
+        end
+      end
+    end
+
+    it "warns when config file is empty" do
+      Dir.mktmpdir do |dir|
+        File.write(File.join(dir, ".rodiff.yml"), "")
+
+        Dir.chdir(dir) do
+          expect { described_class.new }.to output(%r{empty}).to_stderr
+        end
+      end
+    end
+
+    it "warns when config file contains non-Hash data" do
+      Dir.mktmpdir do |dir|
+        File.write(File.join(dir, ".rodiff.yml"), "- item1\n- item2\n")
+
+        Dir.chdir(dir) do
+          expect { described_class.new }.to output(%r{must contain a Hash}).to_stderr
+        end
+      end
+    end
+
+    it "raises error when config file has invalid YAML syntax" do
+      Dir.mktmpdir do |dir|
+        File.write(File.join(dir, ".rodiff.yml"), "invalid: yaml: content:\n  - broken")
+
+        Dir.chdir(dir) do
+          expect { described_class.new }.to raise_error(Rodiff::Error, %r{Invalid YAML})
+        end
+      end
+    end
+
+    it "raises error when config file contains unknown keys" do
+      Dir.mktmpdir do |dir|
+        File.write(File.join(dir, ".rodiff.yml"), "unknown_key: value\n")
+
+        Dir.chdir(dir) do
+          expect { described_class.new }.to raise_error(
+            Rodiff::Configuration::UnknownConfiguration,
+            %r{unknown config :unknown_key}
+          )
         end
       end
     end
